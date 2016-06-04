ab19797e8762ec0f90dba0e908fb7d3227f92fef
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-04 18:43:59 -0500

    Create 189. Rotate Array.cpp

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,56 @@
+// Leetcode.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+//struct ListNode {
+//	int val;
+//	ListNode *next;
+//	ListNode(int x) : val(x), next(NULL) {}
+//	
+//};
+
+class Solution {
+public:
+	void rotate(vector<int>& nums, int k) {
+		int n = nums.size();
+		if ((k <= 0) || (n == 0)) return;
+		k %= n;
+		reverse2(nums, 0, n - k - 1);
+		reverse2(nums, n - k, n - 1);
+		reverse2(nums, 0, n - 1);
+	}
+
+	void reverse2(vector<int>& nums, int start, int end)
+	{
+		int temp;
+		while (start < end)
+		{
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+			start++;
+			end--;
+		}
+
+	}
+};
+int main()
+{
+	Solution sol;
+	vector<int> nums{ 0,1,2,3,4,5,6 };
+	sol.rotate(nums, 3);
+	for (auto i : nums)
+		cout << i << endl;
+	return 0;
+	
+			 
+
+}
