49cf6c29a80814fed1747db6f92416dc0c875b01
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-06 02:48:41 +0800

    Update 125. Valid Palindrome.cpp

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -1,14 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
 class Solution {
 
-public:    
+public:
     bool isPalindrome(string s) {
         auto left = s.begin();
         auto right = prev(s.end());
-        while (left < right) { 
+        while (left < right) {
             if (!isalnum(*left)) {
                 ++left;
             } else if (!isalnum(*right)) {
-                --right; 
+                --right;
             } else if (tolower(*left) != tolower(*right)) {
                 return false;
             } else {
@@ -18,3 +31,19 @@ public:
         return true;
     }
 };
+
+int main(int argc, char *argv[])
+{
+    Solution sol;
+    string s  ="bcdedcb";
+    vector<int> v{4,5,6,7,0,1,2};
+
+    cout << sol.isPalindrome(s) << endl;
+//    cout << sol.findKthLargest(v, 2) << endl;
+//    cout << sol.findKthLargest(v, 3) << endl;
+//    cout << sol.findKthLargest(v, 4) << endl;
+//    cout << sol.findKthLargest(v, 5) << endl;
+//    cout << sol.findKthLargest(v, 6) << endl;
+
+    return 0;
+}
