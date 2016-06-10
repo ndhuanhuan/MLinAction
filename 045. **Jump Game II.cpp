eb7f8917168112957a0e9c3f72b2d0f2d560ccc2
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-10 13:00:56 +0800

    Create 045. **Jump Game II.cpp

---
+++ 045. **Jump Game II.cpp
@@ -0,0 +1,16 @@
+//http://bangbingsyb.blogspot.com/2014/11/leetcode-jump-game-i-ii.html
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int curMax = 0, njumps = 0, i = 0;
+        while(curMax<n-1) {
+            int lastMax = curMax;
+            for(; i<=lastMax; i++) 
+                curMax = max(curMax,i+nums[i]);
+            njumps++;
+            if(lastMax == curMax) return -1;
+        }
+        return njumps;  
+    }
+};
