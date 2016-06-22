551be049afaa826c6517a067c4282556243a4e01
ndhuanhuan <ndhuanhuan@gmail.com>
2016-06-22 12:01:26 +0800

    Create 285. *Inorder Successor in BST.cpp

---
+++ 285. *Inorder Successor in BST.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        // If it has right subtree.
+        if (p && p->right) {
+            p = p->right;
+            while (p->left) {
+                p = p->left;
+            }
+            return p;
+        }
+
+        // Search from root.
+        TreeNode *successor = nullptr;
+        while (root && root != p) { 
+            if (root->val > p->val) {
+                successor = root;
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+
+        return successor;
+    }
+};
