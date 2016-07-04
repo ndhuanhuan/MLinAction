b97a827c060e81e7df6ba91c785ddabf19c91812
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-04 12:52:59 -0500

    Create 133. *Clone Graph.cpp

---
+++ 133. *Clone Graph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	UndirectedGraphNode *cloneGraph(const UndirectedGraphNode *node) {
+		if (node == nullptr) return nullptr;
+		// key is original node􅖌value is copied node
+		unordered_map<const UndirectedGraphNode *,
+			UndirectedGraphNode *> copied;
+		clone(node, copied);
+		return copied[node];
+	}
+private:
+	// DFS
+	static UndirectedGraphNode* clone(const UndirectedGraphNode *node,
+		unordered_map<const UndirectedGraphNode *,
+		UndirectedGraphNode *> &copied) {
+		// a copy already exists
+		if (copied.find(node) != copied.end()) return copied[node];
+		UndirectedGraphNode *new_node = new UndirectedGraphNode(node->label);
+		copied[node] = new_node;
+		for (auto nbr : node->neighbors)
+			new_node->neighbors.push_back(clone(nbr, copied));
+		return new_node;
+	}
+};
