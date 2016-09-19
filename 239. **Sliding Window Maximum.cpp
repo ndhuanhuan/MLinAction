36b0a3247f9f816954776273a2890de9b638c303
ndhuanhuan <ndhuanhuan@gmail.com>
2016-09-19 14:19:56 -0500

    Create 239. **Sliding Window Maximum.cpp

---
+++ 239. **Sliding Window Maximum.cpp
@@ -0,0 +1,16 @@
+//http://www.cnblogs.com/grandyang/p/4656517.html
+//保存数字的下标
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> q;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!q.empty() && q.front() == i - k) q.pop_front();
+            while (!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+            q.push_back(i);
+            if (i >= k - 1) res.push_back(nums[q.front()]);
+        }
+        return res;
+    }
+};
