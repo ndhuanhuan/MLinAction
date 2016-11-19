fe6f0357de91f54a2b8e3349abc36bf3a4f4a771
ndhuanhuan <ndhuanhuan@gmail.com>
2016-11-19 14:06:21 -0600

    Update 053. Maximum Subarray.cpp

--- 053. Maximum Subarray.cpp
+++ 053. Maximum Subarray.cpp
@@ -11,3 +11,23 @@ public:
         return maxSum;
     }
 };
+
+
+//dp
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        const int n = nums.size();
+        vector<int> dp(n,INT_MIN);
+        dp[0] = nums[0];
+        int res = dp[0];
+        
+        for(int i=0;i<n;i++)
+        {
+            dp[i] = nums[i]+(dp[i-1]>0?dp[i-1]:0);
+            res = max(res,dp[i]);
+        }
+        return res;
+    }
+};
