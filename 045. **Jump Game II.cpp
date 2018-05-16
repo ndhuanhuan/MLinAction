472ffec9b6f8b8af129e6db4792dcf6d6492f2a3
ndhuanhuan <ndhuanhuan@gmail.com>
2018-05-16 01:59:07 -0500

    Update 045. **Jump Game II.cpp

--- 045. **Jump Game II.cpp
+++ 045. **Jump Game II.cpp
@@ -14,3 +14,20 @@ public:
         return njumps;  
     }
 };
+
+//http://www.cnblogs.com/grandyang/p/4373533.html
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0, n = nums.size(), last = 0, cur = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            cur = max(cur, i + nums[i]);
+            if (i == last) {
+                last = cur;
+                ++res;
+                if (cur >= n - 1) break;
+            }
+        }
+        return res;
+    }
+};
